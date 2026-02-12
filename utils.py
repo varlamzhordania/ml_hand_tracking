7bbb180f98701ed7181f5a7ff90bcc121cbb7375
Varlam Zhordania <zhordaniavarlam@gmail.com>
2026-02-12 16:43:30 +0400

    add utils.py and preprocess_frame function for optimizing brightness

---
+++ utils.py
@@ -0,0 +1,6 @@
+import cv2
+
+def preprocess_frame(frame):
+    img_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+    img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+    return cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
